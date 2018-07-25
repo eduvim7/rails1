eaf6d21707ad8a1488a5f702586ae6428a1c9b10
eduvim7 <edu@imvim.com>
2018-07-25 17:28:35 +0800

    Implement deleting tickets feature

--- ticketee/app/controllers/tickets_controller.rb
+++ ticketee/app/controllers/tickets_controller.rb
@@ -1,24 +1,29 @@
 class TicketsController < ApplicationController
   before_action :set_project
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
-	def new
+	
+  def new
 	  @ticket = @project.tickets.build
 	end
-	def create
+	
+  def create
   	@ticket = @project.tickets.build(ticket_params)
   	
   	  if @ticket.save
-  		flash[:notice] = "Ticket has been created."
-  		redirect_to [@project, @ticket]
+    		flash[:notice] = "Ticket has been created."
+    		redirect_to [@project, @ticket]
   	  else
-  		flash.now[:alert] = "Ticket has not been created."
-  		render "new"
+    		flash.now[:alert] = "Ticket has not been created."
+    		render "new"
   	  end
-    end
+  end
+  
   def show
   end
+  
   def edit
   end
+  
   def update
     if @ticket.update(ticket_params)
       flash[:notice] = "Ticket has been updated."
@@ -28,6 +33,14 @@ class TicketsController < ApplicationController
       render "edit"
     end
   end
+  
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+
+    redirect_to @project
+  end
+
   private
 
   def ticket_params
